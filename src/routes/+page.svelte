9f53abf85520ee8a7a1274a0b40f90dbd26b6147
KarlFloresca <flarescakarl@gmail.com>
2024-08-27 11:52:19 +0800

    dad
    dada

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script>
   import Layout from "./+layout.svelte";
+
+  export const prerender = true;
+  export const trailingSlash = "always";
 </script>
 
 <main>
--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
 
 export default {
-  kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: 'app.html'
-    }),
-    prerender: {
-      entries: [
-        '/',  
-      ]
-    },
-    paths: {
-      base: process.env.NODE_ENV === 'production' ? '/KarlFloresca' : ''
-    }
-  }
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	}
 };
